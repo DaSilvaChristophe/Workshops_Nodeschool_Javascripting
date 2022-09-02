54a5ae6717bf89adfcd6f4c800e3a3b82f93bb6b
Christophe Da Silva <crisinvest88@gmail.com>
2022-09-02 03:19:10 +0200

    Exercise finish

---
+++ number-to-string.js
@@ -0,0 +1,5 @@
+// toString() transforme les numbers en strings
+
+let n = 128;
+n = n.toString();
+console.log(n);
---
+++ rounding-numbers.js
@@ -0,0 +1,5 @@
+// Math.round (arrondir les numbers décimaux)
+
+let roundUp = 1.5;
+let rounded = Math.round(roundUp);
+console.log(rounded);
